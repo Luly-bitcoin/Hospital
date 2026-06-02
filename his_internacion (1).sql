10fe7a742a5f2e3d96075671d3a5d836073ac773
Luly-bitcoin <villegasmarialuly@gmail.com>
2026-06-02 15:03:29 -0300

    base de datos
    ..

--- his_internacion (1).sql
+++ his_internacion (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-02-2026 a las 21:54:53
+-- Tiempo de generación: 02-06-2026 a las 20:02:55
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -49,7 +49,8 @@ INSERT INTO `admisiones` (`id_admision`, `dni_paciente`, `fecha_ingreso`, `motiv
 (4, 35766234, '2026-02-21 16:56:45', 'Dolor de rodilla', '', 2, NULL, '2026-02-21 16:59:42'),
 (5, 30123456, '2026-02-21 16:59:15', 'Dolor de cabeza', 'activa', 3, NULL, NULL),
 (6, 35766234, '2026-02-21 17:01:09', 'dolores de ovario', '', 1, NULL, '2026-02-21 17:39:53'),
-(7, 35766234, '2026-02-21 17:39:24', 'aehsrjty', 'activa', 2, NULL, NULL);
+(7, 35766234, '2026-02-21 17:39:24', 'aehsrjty', 'activa', 2, NULL, NULL),
+(8, 39887456, '2026-06-02 13:28:08', 'fiebre', 'finalizada', 4, NULL, '2026-06-02 14:02:02');
 
 -- --------------------------------------------------------
 
@@ -85,6 +86,13 @@ CREATE TABLE `altas` (
   `usuario_registra` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Volcado de datos para la tabla `altas`
+--
+
+INSERT INTO `altas` (`id_alta`, `id_admision`, `fecha_alta`, `indicaciones`, `usuario_registra`) VALUES
+(1, 8, '2026-06-02 14:02:02', 'Tomar las pastillas que le resetaron por 1 semana', NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -108,7 +116,31 @@ INSERT INTO `camas` (`id_cama`, `codigo`, `id_habitacion`, `estado`, `sexo_permi
 (1, 'H101-C1', 1, 'higienizando', 'N', 'Cama 1 de hab 101'),
 (2, 'H101-C2', 1, 'ocupada', 'N', 'Cama 2 de hab 101'),
 (3, 'H102-C1', 2, 'ocupada', 'N', 'Cama habitacion 102'),
-(4, 'U01-C1', 3, 'libre', 'N', 'Cama urgencias');
+(4, 'U01-C1', 3, 'ocupada', 'N', 'Cama urgencias');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `emergencias`
+--
+
+CREATE TABLE `emergencias` (
+  `id_emergencia` int(11) NOT NULL,
+  `codigo_temporal` varchar(20) NOT NULL,
+  `sexo` enum('M','F','N') DEFAULT 'N',
+  `descripcion` text DEFAULT NULL,
+  `fecha_ingreso` datetime DEFAULT current_timestamp(),
+  `estado` enum('sin_identificar','identificado','alta') DEFAULT 'sin_identificar',
+  `id_cama` int(11) DEFAULT NULL,
+  `dni_paciente` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `emergencias`
+--
+
+INSERT INTO `emergencias` (`id_emergencia`, `codigo_temporal`, `sexo`, `descripcion`, `fecha_ingreso`, `estado`, `id_cama`, `dni_paciente`) VALUES
+(1, 'EMG-1780423064745', 'F', 'Accidente de moto', '2026-06-02 14:57:44', 'identificado', 4, 39887456);
 
 -- --------------------------------------------------------
 
@@ -136,8 +168,10 @@ CREATE TABLE `evaluacion_enfermeria` (
 --
 
 INSERT INTO `evaluacion_enfermeria` (`id_eval_enf`, `id_admision`, `antecedentes`, `alergias`, `medicamentos`, `motivo_internacion`, `signos_vitales_temp`, `signos_vitales_fc`, `signos_vitales_fr`, `signos_vitales_pa`, `fecha_registro`, `usuario_registra`) VALUES
-(1, 1, 'HTA controlada', 'Ninguna', 'Enalapril', 'Control posoperatorio', 36.7, 78, 18, '120/80', '2025-11-26 16:05:18', 2),
-(2, 4, 'cancer de mama', 'ninguna', 'ninguna', 'dolor de rodilla', 37.3, 63, 13, '120/80', '2026-02-21 16:58:53', NULL);
+(1, 1, 'HTA controlada', 'Ninguna', 'Enalapril', 'Control posoperatorio', 36.7, 78, 18, '120/80', '2025-11-26 16:05:18', NULL),
+(2, 4, 'cancer de mama', 'ninguna', 'ninguna', 'dolor de rodilla', 37.3, 63, 13, '120/80', '2026-02-21 16:58:53', NULL),
+(3, 8, 'ninguno', 'ninguno', 'ninguno', 'fiebre', 40.0, 98, 5, '120/80', '2026-06-02 13:41:02', NULL),
+(4, 8, 'ninguno', 'ninguno', 'ninguno', 'fiebre', 40.0, 98, 5, '120/80', '2026-06-02 13:41:48', NULL);
 
 -- --------------------------------------------------------
 
@@ -161,7 +195,8 @@ CREATE TABLE `evaluacion_medica` (
 --
 
 INSERT INTO `evaluacion_medica` (`id_eval_med`, `id_admision`, `diagnostico`, `estudios`, `tratamiento`, `evolucion`, `fecha_registro`, `usuario_registra`) VALUES
-(1, 1, 'Apendicitis aguda', 'ANALISIS: Hemograma, Rx abdomen', 'Cirugía laparoscópica, analgesia', 'Evoluciona favorable', '2025-11-26 16:05:18', 3);
+(1, 1, 'Apendicitis aguda', 'ANALISIS: Hemograma, Rx abdomen', 'Cirugía laparoscópica, analgesia', 'Evoluciona favorable', '2025-11-26 16:05:18', NULL),
+(2, 8, 'fiebre por intoxicacion', 'ninguno', 'pastillas', 'mejora', '2026-06-02 13:47:27', NULL);
 
 -- --------------------------------------------------------
 
@@ -218,7 +253,8 @@ INSERT INTO `medicos` (`id_medico`, `dni`, `nombre`, `apellido`, `sexo`, `fecha_
 (9, 11111113, 'Laura', 'Gómez', 'F', '1978-07-20', '345678901', 'laura.gomez@hospital.com', 'Cardióloga', 'MAT003', 'activo', '2025-11-27 14:30:20'),
 (10, 11111114, 'Marcos', 'Santos', 'M', '1982-05-05', '456789012', 'marcos.santos@hospital.com', 'Cardiólogo', 'MAT004', 'activo', '2025-11-27 14:30:20'),
 (11, 11111115, 'Carlos', 'Ramírez', 'M', '1990-09-12', '567890123', 'carlos.ramirez@hospital.com', 'Enfermero', 'MAT005', 'activo', '2025-11-27 14:30:20'),
-(12, 11111116, 'Ana', 'López', 'F', '1988-12-01', '678901234', 'ana.lopez@hospital.com', 'Pediatra', 'MAT006', 'activo', '2025-11-27 14:30:20');
+(12, 11111116, 'Ana', 'López', 'F', '1988-12-01', '678901234', 'ana.lopez@hospital.com', 'Pediatra', 'MAT006', 'activo', '2025-11-27 14:30:20'),
+(13, 29667666, 'Gilberto', 'Funes', 'M', '1999-05-07', '2445332211', 'Gilberto@gmail.com', 'Psicologo', 'MAT007', 'activo', '2026-06-02 14:18:55');
 
 -- --------------------------------------------------------
 
@@ -248,9 +284,10 @@ CREATE TABLE `pacientes` (
 
 INSERT INTO `pacientes` (`dni`, `nombre`, `apellido`, `sexo`, `fecha_nacimiento`, `telefono`, `tel_emergencia`, `nombre_dueno`, `direccion`, `localidad`, `edad`, `created_at`, `activo`) VALUES
 (11223344, 'Abel', 'Pintos', 'M', '1984-03-11', '02664000000', '2664123441', 'Mamá de Abel', 'Mitre 222', 'Bahia Blanca', 41, '2025-11-26 21:08:10', 1),
-(30123456, 'Juan', 'Perez', '', '1980-05-12', '2611234567', '2664009876', 'Elsa Castro', 'Calle Falsa 123', 'San Luis', 45, '2025-11-26 16:05:18', 0),
+(30123456, 'Juan', 'Perez', 'M', '1980-05-12', '2611234567', '2664009876', 'Elsa Castro', 'Calle Falsa 123', 'San Luis', 45, '2025-11-26 16:05:18', 0),
 (30987654, 'María', 'Gomez', 'F', '1992-11-30', '2617654321', NULL, NULL, 'Avenida Siempreviva 742', '', 0, '2025-11-26 16:05:18', 1),
 (35766234, 'Velen', 'Calles', 'F', '1999-06-07', '2664990999', '2664887788', 'Celena Gomez', 'Calle cualquiera 222', 'Buenos Aires', 26, '2026-02-21 16:56:02', 1),
+(39887456, 'lala', 'lole', 'F', '2000-05-09', '2665667766', NULL, NULL, NULL, 'La Toma', 26, '2026-06-02 13:23:08', 1),
 (46332709, 'Maria Lourdes', 'Villegas', 'F', '2005-02-17', '02665112822', '2664112348', 'Romina Pereyra', 'Sarmiento 670', 'La Toma', 20, '2025-11-26 20:53:46', 1);
 
 --
@@ -309,7 +346,8 @@ CREATE TABLE `turnos` (
 
 INSERT INTO `turnos` (`id_turno`, `dni_paciente`, `id_medico`, `fecha_solicitada`, `fecha_realizacion`, `estado`, `motivo`, `observaciones`, `usuario_registra`, `created_at`) VALUES
 (1, 46332709, 7, '2025-11-27 18:32:08', '2025-11-30 14:00:00', 'pendiente', 'consulta', NULL, NULL, '2025-11-27 14:33:23'),
-(2, 11223344, 10, '2025-11-29 17:00:00', NULL, 'pendiente', 'dolor de pecho', NULL, 1, '2025-11-27 14:35:26');
+(2, 11223344, 10, '2025-11-29 17:00:00', NULL, 'pendiente', 'dolor de pecho', NULL, NULL, '2025-11-27 14:35:26'),
+(3, 39887456, 13, '2026-06-03 17:00:00', NULL, 'pendiente', 'personales', NULL, NULL, '2026-06-02 14:26:07');
 
 -- --------------------------------------------------------
 
@@ -333,9 +371,7 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id_usuario`, `usuario`, `password`, `nombre`, `email`, `id_rol`, `activo`, `created_at`) VALUES
-(1, 'admin', 'TO_REPLACE_HASH_ADMIN', 'Administrador', 'admin@example.com', 1, 1, '2025-11-26 16:05:18'),
-(2, 'enf1', 'TO_REPLACE_HASH_ENF', 'Enfermeria 1', 'enf1@example.com', 2, 1, '2025-11-26 16:05:18'),
-(3, 'med1', 'TO_REPLACE_HASH_MED', 'Medico 1', 'med1@example.com', 3, 1, '2025-11-26 16:05:18');
+(6, 'Luu', '$2b$10$pI3Cd93HiEUT18k4eptAXOYgGDIQQsLJFgd/8yTnBt/Uf26A4Tx2W', 'Maria Lourdes', 'villegasmarialuly@gmail.com', 2, 1, '2026-06-02 13:22:06');
 
 -- --------------------------------------------------------
 
@@ -399,6 +435,14 @@ ALTER TABLE `camas`
   ADD PRIMARY KEY (`id_cama`),
   ADD UNIQUE KEY `codigo` (`codigo`),
   ADD KEY `idx_camas_habitacion` (`id_habitacion`);
+
+--
+-- Indices de la tabla `emergencias`
+--
+ALTER TABLE `emergencias`
+  ADD PRIMARY KEY (`id_emergencia`),
+  ADD KEY `id_cama` (`id_cama`),
+  ADD KEY `dni_paciente` (`dni_paciente`);
 
 --
 -- Indices de la tabla `evaluacion_enfermeria`
@@ -469,7 +513,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `admisiones`
 --
 ALTER TABLE `admisiones`
-  MODIFY `id_admision` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_admision` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `alas`
@@ -481,7 +525,7 @@ ALTER TABLE `alas`
 -- AUTO_INCREMENT de la tabla `altas`
 --
 ALTER TABLE `altas`
-  MODIFY `id_alta` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_alta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `camas`
@@ -490,16 +534,22 @@ ALTER TABLE `camas`
   MODIFY `id_cama` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
+-- AUTO_INCREMENT de la tabla `emergencias`
+--
+ALTER TABLE `emergencias`
+  MODIFY `id_emergencia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT de la tabla `evaluacion_enfermeria`
 --
 ALTER TABLE `evaluacion_enfermeria`
-  MODIFY `id_eval_enf` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_eval_enf` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `evaluacion_medica`
 --
 ALTER TABLE `evaluacion_medica`
-  MODIFY `id_eval_med` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_eval_med` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `habitaciones`
@@ -511,7 +561,7 @@ ALTER TABLE `habitaciones`
 -- AUTO_INCREMENT de la tabla `medicos`
 --
 ALTER TABLE `medicos`
-  MODIFY `id_medico` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_medico` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `roles`
@@ -523,13 +573,13 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT de la tabla `turnos`
 --
 ALTER TABLE `turnos`
-  MODIFY `id_turno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_turno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Restricciones para tablas volcadas
@@ -555,6 +605,13 @@ ALTER TABLE `altas`
 --
 ALTER TABLE `camas`
   ADD CONSTRAINT `fk_cama_habitacion` FOREIGN KEY (`id_habitacion`) REFERENCES `habitaciones` (`id_habitacion`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `emergencias`
+--
+ALTER TABLE `emergencias`
+  ADD CONSTRAINT `emergencias_ibfk_1` FOREIGN KEY (`id_cama`) REFERENCES `camas` (`id_cama`),
+  ADD CONSTRAINT `emergencias_ibfk_2` FOREIGN KEY (`dni_paciente`) REFERENCES `pacientes` (`dni`);
 
 --
 -- Filtros para la tabla `evaluacion_enfermeria`
